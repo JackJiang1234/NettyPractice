a781fb9652cd974b976771e765238600c9a04e54
jack <jjy_8888@163.com>
2021-05-22 17:39:09 +0800

    Revert "Revert "add redis practice""
    This reverts commit 46b2621fa3c911df61819f2d422c888a532a62f7.

---
+++ redis/src/main/java/com/netty/practice/RedisReplyEncoder.java
@@ -0,0 +1,16 @@
+package com.netty.practice;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class RedisReplyEncoder extends MessageToByteEncoder<RedisReply> {
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, RedisReply msg, ByteBuf out) throws Exception {
+        System.out.println("RedisReplyEncoder: " + msg);
+        msg.write(out);
+    }
+
+}
+
